3922276b8fbe6b6a1e81d98975a7d6c8e486b4b7
deni-cyber <deniskamau928@gmial.com>
2025-09-17 01:54:45 +0300

    first commit

---
+++ javascript/main.js
@@ -0,0 +1,33 @@
+  const toggleBtn = document.getElementById('navToggle');
+  const sideNav = document.getElementById('sideNav');
+
+  toggleBtn.addEventListener('click', () => {
+    sideNav.classList.toggle('open');
+  });
+
+  document.addEventListener('click', (e) => {
+  if (!sideNav.contains(e.target) && !toggleBtn.contains(e.target)) {
+    sideNav.classList.remove('open');
+  }
+});
+
+  document.addEventListener("DOMContentLoaded", function () {
+    const intro = document.querySelector(".intro-section");
+    const images = [
+      "staticimages/installationbg.jpg",
+      "staticimages/installationbg1.jpeg",
+    ];
+
+    let i = 0;
+
+    function changeBackground() {
+      intro.style.backgroundImage = `url('${images[i]}')`;
+      i = (i + 1) % images.length;
+    }
+
+    // Initial load
+    changeBackground();
+
+    // Change every 5 seconds
+    setInterval(changeBackground, 5000);
+  });
