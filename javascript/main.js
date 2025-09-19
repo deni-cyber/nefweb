44d1c23a44cb455db3fb0e1b0407dad9e57ce8bb
deni-cyber <deniskamau928@gmial.com>
2025-09-19 17:38:59 +0300

    polished the layout

--- javascript/main.js
+++ javascript/main.js
@@ -1,34 +1,64 @@
-  const toggleBtn = document.getElementById('navToggle');
-  const sideNav = document.getElementById('sideNav');
 
-  toggleBtn.addEventListener('click', () => {
-    sideNav.classList.toggle('open');
-  });
+//opening the sidebar
+const toggleBtn = document.getElementById('navToggle');
+const sideNav = document.getElementById('sideNav');
 
-  document.addEventListener('click', (e) => {
+toggleBtn.addEventListener('click', () => {
+  sideNav.classList.toggle('open');
+  toggleBtn.classList.toggle('open'); // this line toggles the cross
+});
+
+document.addEventListener('click', (e) => {
   if (!sideNav.contains(e.target) && !toggleBtn.contains(e.target)) {
     sideNav.classList.remove('open');
+    toggleBtn.classList.remove('open'); // reset back to hamburger
   }
 });
 
-  document.addEventListener("DOMContentLoaded", function () {
-    const intro = document.querySelector(".intro-section");
-    const images = [
-      "staticimages/welding.jpg",
-      "staticimages/metalcutting.jpg",
-      "staticimages/metalbending.jpg",
-    ];
 
-    let i = 0;
 
-    function changeBackground() {
-      intro.style.backgroundImage = `url('${images[i]}')`;
-      i = (i + 1) % images.length;
-    }
 
-    // Initial load
-    changeBackground();
+// changing background
+document.addEventListener("DOMContentLoaded", function () {
+  const intro = document.querySelector(".intro-section");
+  const images = [
+    "staticimages/weld.jpg",
+    "staticimages/metalcutting.jpg",
+    "staticimages/metalbending.jpg",
+  ];
 
-    // Change every 5 seconds
-    setInterval(changeBackground, 5000);
-  });
+  let i = 0;
+
+  function changeBackground() {
+    intro.style.backgroundImage = `url('${images[i]}')`;
+    i = (i + 1) % images.length;
+  }
+
+  // Initial load
+  changeBackground();
+
+  // Change every 5 seconds
+  setInterval(changeBackground, 5000);
+});
+
+
+
+
+
+
+//sending message via whatsapp
+document.getElementById("contactForm").addEventListener("submit", function(e) {
+  e.preventDefault();
+
+  const name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+  const message = document.getElementById("message").value;
+
+  // Replace with your WhatsApp number (with country code, no + or spaces)
+  const phone = "+2547 21394475";
+
+  const text = `Hello, my name is ${name} (%20${email}%20), and am reaching out from your website.Message: %0A${message}`;
+  const url = `https://wa.me/${phone}?text=${text}`;
+
+  window.open(url, "_blank");
+});
